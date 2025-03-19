758b54589a13e132f0c45d2715e5027bbace4111
sarvesh <sarvesh@fplabs.tech>
2025-03-19 13:26:23 +0530

    first commit

---
+++ src/main/java/com/scaler/capstone/user/ScalerCapstoneUserApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.capstone.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalerCapstoneUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalerCapstoneUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scaler/capstone/user/ScalerCapstoneUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.capstone.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalerCapstoneUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
